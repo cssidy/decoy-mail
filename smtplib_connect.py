99648e7649486b7807edfb16844292fb9e300142
Cassidy Brooke <cassidy@cassidybrooke.net>
2016-09-19 21:51:22 -0400

    Create smtplib_connect.py

---
+++ smtplib_connect.py
@@ -0,0 +1,59 @@
+#! /usr/bin/python3
+
+import smtplib
+from imaplib_fetch_rfc822 import from_, subject_
+from pattern_match import email_result
+
+if email_result is True:
+    print("TRUE")
+
+def smtp_connection(verbose=True):
+    # ENTER YOUR EMAIL
+    email_account = 'xxxxx@gmail.com'
+    # ENTER YOUR PASSWORD
+    password = 'xxxxx'
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    print('Logging in as', email_account)
+    server.login(email_account, password)
+    print('Log in success.')
+    print('\n')
+
+    def compose_email(email_result):
+        sender = email_account
+        to = from_
+        # why is this breaking into single characters with a comma between each?
+        # need to remove the From: that is at the beginning of from_
+        subject = ('Re: ' + subject_)
+        # this is not displaying, empty subject line
+        # clean this up, Subject: Re: Subject: Want to go to the beach Sunday?
+        body = "Yes, that would be delightful."
+        # why is there a space at the beginning?
+
+        email_text = """\
+            From: %s
+            To: %s
+            Subject: %s
+
+            %s
+            """ % (sender, ", ".join(to), subject, body)
+
+        try:
+            server.sendmail(sender, to, email_text)
+            server.close()
+
+            print('Email delivered.')
+
+        except:
+            print('Unable to send email.')
+
+    compose_email(email_result)
+
+    return server
+    server.logout()
+
+# prints results of def open_connection
+if __name__ == '__main__':
+    with smtp_connection(verbose=True) as c:
+        print(c)
+
