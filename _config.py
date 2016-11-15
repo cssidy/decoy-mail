5d96eba84067c0062f555d64dae59b82afe5af51
Cassidy Brooke <cassidy@cassidybrooke.net>
2016-11-15 13:05:56 -0500

    Update _config.py

--- _config.py
+++ _config.py
@@ -1,3 +1,4 @@
-# HOSTNAME = .gmail.com
-# USERNAME = joe@gmail.com
-# PASSWORD = holymackerel
+# this file will not be uploaded to Git
+
+config_dict = {'IMAP_HOSTNAME': 'imap.gmail.com', 'SMTP_HOSTNAME': 'smtp.gmail.com', 'USERNAME':
+    'example@gmail.com', 'PASSWORD': 'password'}
