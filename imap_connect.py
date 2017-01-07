c7c18864a2cca6ccf9e026d6bb470b3a1d08eec3
cssidy <cassidy@cassidybrooke.net>
2017-01-06 19:08:14 -0500

    streamlined imap_connect.py

--- imap_connect.py
+++ imap_connect.py
@@ -2,32 +2,21 @@
 
 import imaplib
 from login_credentials import gmail_credentials
-# import configparser
-# import os
 
 
-def open_connection(verbose=True):
-    # Read the config file
-    # config = configparser.ConfigParser()
-    # config.read([os.path.expanduser('~/.pymotw')])
-
-    # Connect to the server
-    hostname = ('imap.gmail.com')
+def open_imap_connection(verbose=True):
+    # Connect to the imap server
+    hostname = 'imap.gmail.com'
     if verbose:
-        print('\n')
-        print('Connecting to', hostname)
-    server = imaplib.IMAP4_SSL(hostname)
+       print('\n')
+       print('Connecting to', hostname)
+    imap_server = imaplib.IMAP4_SSL(hostname)
 
-    # Login to our account
-    email_account = ()
-    password = ()
+    # Login to email account
     if verbose:
         print('Logging in as', gmail_credentials.get('username', 0))
-        print('\n')
-    server.login(gmail_credentials.get('username', 0), gmail_credentials.get('password', 0))
-    return server
+    imap_server.login(gmail_credentials.get('username', 0), gmail_credentials.get('password', 0))
+    print('Success.')
 
-# prints results of def open_connection
-if __name__ == '__main__':
-    with open_connection(verbose=True) as c:
-        print(c)
+
+
