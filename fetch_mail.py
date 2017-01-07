3c5772706b06dda4c654539ddd34ccaea80aa090
cssidy <cassidy@cassidybrooke.net>
2017-01-06 19:19:39 -0500

    re-writing fetch_mail.py

--- fetch_mail.py
+++ fetch_mail.py
@@ -1,23 +1,13 @@
 #! /usr/bin/python3
 
-import imaplib
-import imap_connect
+# from imap_connect import start_imap_connection
 
-with imap_connect.open_connection() as c:
-    c.select('INBOX')
-    typ, [msg_ids] = c.search(None, 'TEXT', 'Sunday')
-    for num in msg_ids.split():
-        typ, msg_data = c.fetch(num, '(RFC822)')
-        for raw_email in msg_data:
-            # raw_email is a tuple of len==2, we need the 2nd item:
-            if len(raw_email) >= 2:
-                b = raw_email[1]
-                email = b.decode('utf-8').split('\r\n')
-                subject_, from_, to_, body_, = ' ' + email[0], email[1], email[2], 'Body: ' + email[4]
-                print(subject_, '\n', from_, '\n', body_, '\n')
-            else:
-                break
-                # Use this for debugging if the raw_email is diff't length
+# initiate function call for start_imap_connection
 
+# search for emails to bait that are in inbox and spam
+
+# fetch content from emails (from, subject, body) and store
 
 # close imap server connection
+# imap_server.quit()
+
